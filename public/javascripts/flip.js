e0d09f435863a08801b694e74a8d677e24d48daa
Coburn Berry <Coburn.d.berry@gmail.com>
2014-07-01 21:33:31 -0500

    mostly abstracted I think..

--- public/javascripts/flip.js
+++ public/javascripts/flip.js
@@ -1,14 +1,29 @@
-$(
-  $('.page').mouseenter( function(event){
- 		if( !$(event.target).hasClass('displayed')){
- 			$('.page').each(function(pg){
+$(function(){
+	accordion = function(el){
+		var dSize = ((el.children().length -1)/el.children().length) * 100
+		var pSize = ((100 -dSize)/(el.children().length -1))
+		var newRule = ".displayed { width: "+ dSize+"vw }"
+		var pageRule =".page {\
+			width: "+ pSize +"vw;\
+			-webkit-transition: all 1s ease-in-out;\
+			transition: all 1s ease-in-out;\
+			overflow-y: hidden;\
+			margin: auto;\
+			display: inline;\
+			position: relative;}"
+		$('<style type="text/css"></style>').text(pageRule).appendTo('head');
+		$('<style type="text/css"></style>').text(newRule).appendTo('head');	
+	
+  $('.page').mouseenter(function (event) {
+ 		if( !$(event.target).hasClass('displayed') &&
+		$(event.currentTarget).prop("tagName") === "DIV"){
+ 			$('.page').each(function (pg) {
  				if ($(this).hasClass('displayed')){
  					$(this).removeClass('displayed')
  				}
- 			
- 		  $(event.target).addClass('displayed');
- 			
  			})
+ 		  $(event.currentTarget).addClass('displayed')
  		}
  	})
-)
+ }
+})
